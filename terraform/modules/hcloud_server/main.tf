7bd73264cb800f6a137458721106cc20d7af30c5
Andy Cockerill <cockerill.andrew@gmail.com>
2021-12-09 20:58:50 +0000

    changes

--- terraform/modules/hcloud_server/main.tf
+++ terraform/modules/hcloud_server/main.tf
@@ -12,7 +12,16 @@ resource "hcloud_server" "server" {
   image       = data.hcloud_image.github_runner.id #The ID is displayed when the image is created
   server_type = var.server_type
   location    = var.location
-  user_data   = "${file("files/cloud-init.yaml")}"
+  user_data   = <<EOF
+
+  #cloud-config
+  runcmd:
+  - cd ${runner_home}
+  - sudo -u var.runner_user ./config.sh --unattended var.extra_flags --name output.hcloud_server.server.id --url "https://github.com/$GH_REPO" --token var.runner_token
+  - ./svc.sh install var.runner_user
+  - ./svc.sh start
+
+  EOF
 
   network {
       network_id = var.network_id
--- terraform/modules/hcloud_server/variables.tf
+++ terraform/modules/hcloud_server/variables.tf
@@ -1,7 +1,10 @@
-variable "type"        {}
-variable "server_type" {}
-variable "location"    {}
-variable "network_id"  {}
-variable "runner_home" {}
-variable "runner_user" {}
-variable "extra_flags" {}
+variable "type"         {}
+variable "server_type"  {}
+variable "location"     {}
+variable "network_id"   {}
+variable "runner_home"  {}
+variable "runner_user"  {}
+variable "extra_flags"  {}
+variable "hcloud_token" {
+  sensitive = true
+}
