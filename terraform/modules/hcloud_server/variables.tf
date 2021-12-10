1f83f0038747dc57c79958e22edd16c6182687a8
Andy Cockerill <cockerill.andrew@gmail.com>
2021-12-10 22:06:31 +0000

    stop

--- terraform/modules/hcloud_server/variables.tf
+++ terraform/modules/hcloud_server/variables.tf
@@ -1,12 +1,41 @@
-variable "type"         {}
-variable "server_type"  {}
-variable "location"     {}
-variable "network_id"   {}
-variable "runner_home"  {}
-variable "runner_user"  {}
-variable "extra_flags"  {}
-variable "gh_repo"      {}
-variable "runner_token" {}
+variable "type"         {
+  type = string
+  nullable = true
+}
+variable "server_type"  {
+  type = string
+  nullable = true
+}
+variable "location"     {
+  type = string
+  nullable = true
+}
+variable "network_id"   {
+  type = string
+  nullable = true
+}
+variable "runner_home"  {
+  type = string
+  nullable = true
+}
+variable "runner_user"  {
+  type = string
+  nullable = true
+}
+variable "extra_flags"  {
+  type = string
+  nullable = true
+}
+variable "gh_repo"      {
+  type = string
+  nullable = true
+}
+variable "runner_token" {
+  type = string
+  nullable = true
+}
 variable "hcloud_token" {
   sensitive = true
+  type = string
+  nullable = false
 }
