c22b14477bec396c59af7a064d564d7b28c9d87b
Andy Cockerill <cockerill.andrew@gmail.com>
2021-12-10 11:45:10 +0000

    removed vars

--- terraform/modules/hcloud_server/variables.tf
+++ terraform/modules/hcloud_server/variables.tf
@@ -2,9 +2,6 @@ variable "type"         {}
 variable "server_type"  {}
 variable "location"     {}
 variable "network_id"   {}
-variable "runner_home"  {}
-variable "runner_user"  {}
-variable "extra_flags"  {}
 variable "hcloud_token" {
   sensitive = true
 }
