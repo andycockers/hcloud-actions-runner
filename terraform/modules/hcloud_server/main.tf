726ac7fa6a58870c302ee52e96585c0a45dfb7bf
Andy Cockerill <cockerill.andrew@gmail.com>
2021-12-10 13:20:22 +0000

    changes

--- terraform/modules/hcloud_server/main.tf
+++ terraform/modules/hcloud_server/main.tf
@@ -16,7 +16,7 @@ resource "hcloud_server" "server" {
 
   #cloud-config
   runcmd:
-  - cd ${runner_home}
+  - cd var.runner_home
   - sudo -u var.runner_user ./config.sh --unattended var.extra_flags --name output.hcloud_server.server.id --url "https://github.com/$GH_REPO" --token var.runner_token
   - ./svc.sh install var.runner_user
   - ./svc.sh start
--- terraform/modules/hcloud_server/variables.tf
+++ terraform/modules/hcloud_server/variables.tf
@@ -2,6 +2,8 @@ variable "type"         {}
 variable "server_type"  {}
 variable "location"     {}
 variable "network_id"   {}
+variable "runner_home"  {}
+variable "runner_user"  {}
 variable "hcloud_token" {
   sensitive = true
 }
