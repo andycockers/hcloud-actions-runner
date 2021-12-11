e508c6895d041eb1717be69a8b7ce1afe6c2f1e4
Andy Cockerill <cockerill.andrew@gmail.com>
2021-12-11 13:50:59 +0000

    removed ssh key

--- terraform/modules/hcloud_server/main.tf
+++ terraform/modules/hcloud_server/main.tf
@@ -12,12 +12,11 @@ resource "hcloud_server" "server" {
   image       = data.hcloud_image.github_runner.id #The ID is displayed when the image is created
   server_type = var.server_type
   location    = var.location
-  ssh_keys    = ["andy@DESKTOP-CC1QGR9"]
   user_data   = <<EOF
 
   #cloud-config
   runcmd:
-    - #cd "${var.runner_home}"
+    - cd "${var.runner_home}"
     - instance_id=$(cat /var/lib/cloud/data/instance-id)
     - sudo -u "${var.runner_user}" ./config.sh --unattended "${var.extra_flags}" --name "$instance_id" --url "https://github.com/${var.gh_repo}" --token "${var.runner_token}" --labels "$instance_id"
     - ./svc.sh install "${var.runner_user}"
